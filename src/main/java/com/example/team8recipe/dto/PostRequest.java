0ae7ca56fcfd54d94b61727fcdcc543b99809e98
MSM <qw12345611@naver.com>
2023-06-30 17:53:14 +0900

    Dto 생성

--- src/main/java/com/example/team8recipe/dto/PostRequest.java
+++ src/main/java/com/example/team8recipe/dto/PostRequest.java
@@ -1,4 +1,9 @@
 package com.example.team8recipe.dto;
 
+import lombok.Getter;
+
+@Getter
 public class PostRequest {
+    private String title;
+    private String content;
 }
--- src/main/java/com/example/team8recipe/dto/PostResponse.java
+++ src/main/java/com/example/team8recipe/dto/PostResponse.java
@@ -1,4 +1,14 @@
 package com.example.team8recipe.dto;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class PostResponse {
+    private long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedDate;
 }
