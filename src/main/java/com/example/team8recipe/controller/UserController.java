b333913a9930ee979fc73a292cb2237d71d91b89
Choo Hyunjoong <hjchoo19950327@gmail.com>
2023-07-03 20:05:46 +0900

    로그아웃 기능 구현

--- src/main/java/com/example/team8recipe/controller/UserController.java
+++ src/main/java/com/example/team8recipe/controller/UserController.java
@@ -2,26 +2,35 @@ package com.example.team8recipe.controller;
 
 import com.example.team8recipe.dto.SignupRequestDto;
 import com.example.team8recipe.entity.User;
+import com.example.team8recipe.jwt.JwtUtil;
 import com.example.team8recipe.serrvice.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/signup")
     public User userSignup(@Valid @RequestBody SignupRequestDto requestDto){
        return userService.userSignup(requestDto);
     }
+
+    @GetMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response, String name) {
+
+        jwtUtil.deleteCookie(request, response, "Authorization");
+    }
+
 
 
 }
