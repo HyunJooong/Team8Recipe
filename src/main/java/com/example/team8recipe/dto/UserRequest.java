d08532370451c4f0a6b62e379d1e5b560496a03e
MSM <qw12345611@naver.com>
2023-07-03 09:58:46 +0900

    UserRequest,Response 추가

---
+++ src/main/java/com/example/team8recipe/dto/UserRequest.java
@@ -0,0 +1,10 @@
+package com.example.team8recipe.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserRequest {
+    private String password;
+    private String userName;
+    private String intro;
+}
