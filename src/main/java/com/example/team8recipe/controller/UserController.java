39cdee001efecdd18b3cca6dccca97dcbeb7e733
LEEMINWOO <mw7328@naver.com>
2023-07-03 14:06:07 +0900

    Spring Security 회원가입/로그인 & 인증/인가 추가

--- src/main/java/com/example/team8recipe/controller/UserController.java
+++ src/main/java/com/example/team8recipe/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.example.team8recipe.controller;
 
+import com.example.team8recipe.dto.SignupRequestDto;
+import com.example.team8recipe.entity.User;
+import com.example.team8recipe.serrvice.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public User userSignup(@Valid @RequestBody SignupRequestDto requestDto){
+       return userService.userSignup(requestDto);
+    }
+
+
 }
--- src/main/java/com/example/team8recipe/serrvice/UserService.java
+++ src/main/java/com/example/team8recipe/serrvice/UserService.java
@@ -1,4 +1,43 @@
 package com.example.team8recipe.serrvice;
 
+import com.example.team8recipe.dto.LoginRequestDto;
+import com.example.team8recipe.dto.SignupRequestDto;
+import com.example.team8recipe.entity.User;
+import com.example.team8recipe.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    //사용자 회원가입
+    public User userSignup(SignupRequestDto requestDto) {
+        String userId = requestDto.getUserId();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+        String username = requestDto.getUsername();
+        String intro = requestDto.getIntro();
+
+        //유저 아이디 확인
+        Optional<User> checkUsername = userRepository.findByUserId(userId);
+        if (checkUsername.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+
+        User user = new User(userId, password, username, intro);
+        userRepository.save(user);
+
+        return user;
+    }
+
 }
