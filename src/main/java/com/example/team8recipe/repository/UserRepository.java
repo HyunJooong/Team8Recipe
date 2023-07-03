36de30d42a9712f860fe0f4dacc05d23e4cd79ac
minwoolee7328 <131872877+minwoolee7328@users.noreply.github.com>
2023-07-03 14:31:38 +0900

    Revert "Spring Security 회원가입/로그인 & 인증/인가 추가"

--- src/main/java/com/example/team8recipe/repository/UserRepository.java
+++ src/main/java/com/example/team8recipe/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.example.team8recipe.repository;
 import com.example.team8recipe.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+public interface UserRepository {
 
-public interface UserRepository extends JpaRepository<User,Long>{
-    Optional<User> findByUserId(String userId);
 }
