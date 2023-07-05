2fa370be0c0304f428c982176cb5c03423bf9aea
MSM <qw12345611@naver.com>
2023-07-05 22:46:44 +0900

    이메일 프론트엔드 구현완료

--- src/main/java/com/example/team8recipe/controller/UserController.java
+++ src/main/java/com/example/team8recipe/controller/UserController.java
@@ -43,8 +43,6 @@ public class UserController {
     @PostMapping("/signup/checkCode")
     @ResponseBody
     public String checkCode(@RequestParam String code) {
-        // 실제로는 서버에서 저장한 인증번호와 사용자가 입력한 인증번호를 비교하여 처리해야합니다.
-        // 여기서는 단순히 입력한 인증번호와 동일한지만 확인하는 예시입니다.
         if (code.equals(emailService.getEPw())) {
             return "success";
         } else {
