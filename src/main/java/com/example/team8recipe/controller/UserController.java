a954d843c0ceed05fc2ece4eb72f0f3efc008f71
LEEMINWOO <mw7328@naver.com>
2023-07-06 02:18:46 +0900

    이메일인증 프론트앤드 구성

--- src/main/java/com/example/team8recipe/controller/UserController.java
+++ src/main/java/com/example/team8recipe/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.team8recipe.dto.SignupRequestDto;
 import com.example.team8recipe.jwt.JwtUtil;
 import com.example.team8recipe.service.EmailService;
 import com.example.team8recipe.service.UserService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -11,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URLEncoder;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -26,11 +29,6 @@ public class UserController {
         return "signup";
     }
 
-    @GetMapping("/posts-page")
-    public String postsSignup(){
-        return "posts";
-    }
-
     @PostMapping("/signup")
     public String userSignup(@Valid @ModelAttribute SignupRequestDto requestDto){
         userService.userSignup(requestDto);
@@ -41,18 +39,33 @@ public class UserController {
     @PostMapping("/signup/mailConfirm")
     @ResponseBody
     public void mailConfirm(@RequestParam String email, HttpServletResponse response) throws Exception {
-           emailService.sendSimpleMessage(email);
+
+
+        if(email.matches(".*@.*")){
+            String code = emailService.sendSimpleMessage(email);
+            log.info("인증코드 : " + code);
+
+            response.setStatus(200);
+        }else {
+            System.out.println("emailService.getEPw() = " + emailService.getEPw());
+            String code = email;
+            log.info("사용자 입력코드 : " + code);
+
+            if(!emailService.getEPw().equals(code)){
+                response.setStatus(401);
+            }else {
+                response.setStatus(200);
+            }
+
+        }
     }
 
+    @PostMapping("/signup/success")
+    public void success(@RequestParam String email, HttpServletResponse response){
+        String code = email;
+        log.info("사용자 입력코드 : " + code);
 
-    @PostMapping("/signup/checkCode")
-    @ResponseBody
-    public String checkCode(@RequestParam String code) {
-        if (code.equals(emailService.getEPw())) {
-            return "success";
-        } else {
-            return "failure";
-        }
+//        response.setStatus(401);
     }
 
    @GetMapping("/login-page")
@@ -64,16 +77,6 @@ public class UserController {
     public String logout(HttpServletRequest request, HttpServletResponse response,String name){
        jwtUtil.deleteCookie(request,response,"Authorization");
        return "redirect:/";
-    }
-
-    @GetMapping("/profile-page")
-    public String getProfilePage() {
-        return "profile";
-    }
-
-    @GetMapping("/profile-page/pwchange")
-    public String getPwChangePage() {
-        return "pwchange";
     }
 
 }
