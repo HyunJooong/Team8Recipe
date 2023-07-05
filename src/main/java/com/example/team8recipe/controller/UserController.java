fccefcadb2480b6999650e82ccb5b1dc9cbde3a4
LEEMINWOO <mw7328@naver.com>
2023-07-06 02:18:46 +0900

    이메일인증 프론트앤드 구성

--- src/main/java/com/example/team8recipe/controller/UserController.java
+++ src/main/java/com/example/team8recipe/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.team8recipe.dto.SignupRequestDto;
 import com.example.team8recipe.jwt.JwtUtil;
 import com.example.team8recipe.service.EmailService;
 import com.example.team8recipe.service.UserService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -11,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URLEncoder;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,6 +23,7 @@ public class UserController {
     private final UserService userService;
     private final EmailService emailService;
     private final JwtUtil jwtUtil;
+
     @GetMapping("/signup-page")
     public String userSignup(){
         return "signup";
@@ -34,10 +38,34 @@ public class UserController {
     // 이메일 인증
     @PostMapping("/signup/mailConfirm")
     @ResponseBody
-    public String mailConfirm(@RequestParam String email) throws Exception {
-        String code = emailService.sendSimpleMessage(email);
-        log.info("인증코드 : " + code);
-        return code;
+    public void mailConfirm(@RequestParam String email, HttpServletResponse response) throws Exception {
+
+
+        if(email.matches(".*@.*")){
+            String code = emailService.sendSimpleMessage(email);
+            log.info("인증코드 : " + code);
+
+            response.setStatus(200);
+        }else {
+            System.out.println("emailService.getEPw() = " + emailService.getEPw());
+            String code = email;
+            log.info("사용자 입력코드 : " + code);
+
+            if(!emailService.getEPw().equals(code)){
+                response.setStatus(401);
+            }else {
+                response.setStatus(200);
+            }
+
+        }
+    }
+
+    @PostMapping("/signup/success")
+    public void success(@RequestParam String email, HttpServletResponse response){
+        String code = email;
+        log.info("사용자 입력코드 : " + code);
+
+//        response.setStatus(401);
     }
 
     @PostMapping("/signup/checkCode")
